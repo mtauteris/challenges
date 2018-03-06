4f5e4ab40ababeebb3735b9b5403d03cc4ef1616
mtauteris <mindaugas.tauteris@gmail.com>
2018-03-06 20:21:46 +0200

    line to int array

--- Challenge#350_[Easy]_Bookshelf_problem/src/MyDataReader.java
+++ Challenge#350_[Easy]_Bookshelf_problem/src/MyDataReader.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**
  * Created by Minde on 2018.03.06.
@@ -8,15 +10,22 @@ import java.io.IOException;
 public class MyDataReader {
     private static final String data = "data.txt";
     private static String firstLine = null;
+    private static String[] bookShelfLength = null;
+    private static int[] bookShelfLengthInt = null;
+
     public void read(){
         BufferedReader bufferedReader = null;
         FileReader fileReader = null;
         try {
             fileReader = new FileReader(data);
             bufferedReader = new BufferedReader(fileReader);
-
             firstLine = bufferedReader.readLine();
-            System.out.println(firstLine);
+            bookShelfLength=firstLine.split(" ");
+            bookShelfLengthInt = new int[bookShelfLength.length];
+            for (int i= 0; bookShelfLength.length>i;i++){
+                bookShelfLengthInt[i]=Integer.valueOf(bookShelfLength[i]);
+                System.out.println(bookShelfLengthInt[i]);
+            }
         }
         catch (IOException ex){
             System.out.println("klaida "+ ex);
